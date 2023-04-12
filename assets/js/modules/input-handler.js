e583425faa0a9e1999d9f3fed40a87413c2340ca
kevindasilva1603 <kevinsilva1603@gmail.com>
2023-04-12 17:28:45 +0200

    update2.0

---
+++ assets/js/modules/input-handler.js
@@ -0,0 +1,9 @@
+export class InputHandler {}
+export const Key = Object.freeze({
+    ArrowDown: 'ArrowDown',
+    ArrowDown: 'ArrowUp',
+    ArrowDown: 'ArrowLeft',
+    ArrowDown: 'ArrowRight',
+    Space: 'Space',
+    Enter: 'Enter',
+})
