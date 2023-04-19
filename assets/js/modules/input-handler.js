bca532aec7a63cc845290cf72dd1cc97aa5b5b6c
kevindasilva1603 <kevinsilva1603@gmail.com>
2023-04-19 14:20:40 +0200

    Update2.0

--- assets/js/modules/input-handler.js
+++ assets/js/modules/input-handler.js
@@ -1,6 +1,17 @@
 export class InputHandler {
     constructor() {
         /** @type {Set<Key>} */ this.keys = new Set()
+        window.addEventListener('keydown', ({ code }) => {
+            // if (
+            //     code === Key.ArrowDown
+            //     code === Key.ArrowUp
+            //     code === Key.ArrowLeft
+            //     code === Key.ArrowRight
+            //     code === Key.Space
+            //     code === Key.Enter)
+            if (Object.keys(Key).includes(code)) this.keys.add(code)
+        })
+        window.addEventListener('keyup', ({ code }) => this.keys.delete(code))
     }
 }
 
